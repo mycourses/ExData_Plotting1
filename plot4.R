b0974ccfbd8f19c9d1f69765c910ad3987af6102
mycourses <--global>
2014-05-12 01:44:29 +0200

    Final commit - added plot 4

--- plot4.R
+++ plot4.R
@@ -15,13 +15,28 @@ tm <- hpc$Time
 ts <- paste(dt, tm)
 hpc$Timestamp <- strptime(ts, "%d/%m/%Y %H:%M:%S")
 
-## Plot the time series lines (Plot 3)
+
+## Initialise the four plot layout
+par(mfcol=c(2, 2))
+
+## Plot the time series line (from Plot 2) - Top left corner
+plot(hpc$Timestamp, hpc$Global_active_power, type="l", ann=FALSE)
+title(ylab="Global Active Power (kilowatts)")
+
+## Plot the time series lines (from Plot 3) - Bottom left corner
 plot(hpc$Timestamp, hpc$Sub_metering_1, type="l", col="black", ann=FALSE)
 lines(hpc$Timestamp, hpc$Sub_metering_2, type="l", col="red")
 lines(hpc$Timestamp, hpc$Sub_metering_3, type="l", col="blue")
 title(ylab="Energy sub metering")
 legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
-       col=c("black", "red", "blue"), lty=1, lwd=2, cex=0.8)
+       col=c("black", "red", "blue"), lty=1, lwd=2, cex=0.4)
 
+## Plot the time series Voltage - Top right corner
+plot(hpc$Timestamp, hpc$Voltage, type="l", ann=FALSE)
+title(xlab="datetime", ylab="Voltage")
+
+## Plot the time series Global Reactive Power - Bottom right corner
+plot(hpc$Timestamp, hpc$Global_reactive_power, type="l", ann=FALSE)
+title(xlab="datetime", ylab="Global_reactive_power")
 
 
